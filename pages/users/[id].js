60f987795224363ce4d13f3e99f70d6c3b0a6063
AbdulazizSulaymon <djshahrick@gmail.com>
2021-08-16 11:04:01 +0500

    123

---
+++ pages/users/[id].js
@@ -0,0 +1,15 @@
+import { Router, useRouter } from 'next/dist/client/router'
+import React from 'react'
+
+const User = () => {
+    const router = useRouter();
+    const { id } = router.query;
+
+    return (
+        <div className="p-5">
+            <h1 className="text-center">id: {id}</h1>
+        </div>
+    )
+}
+
+export default User
